9b9d8ef4815ce6a85f1d79990f0d25751802bc80
Alejandro Gaviria <mr.gaviria@gmail.com>
2018-02-03 01:30:57 -0500

    Added read and write methods to send and receive messages.  Included 3 new types to room struct (join & leave channels) and (a map of all clients in a room).

--- client.go
+++ client.go
@@ -1,6 +1,6 @@
 package main
 
-import "golang.org/x/net/websocket"
+import "github.com/gorilla/websocket"
 
 // client represents a single chatterbox user
 type client struct {
@@ -12,9 +12,39 @@ type client struct {
 	room *room
 }
 
+// read allows our client to read from the websocket via the ReadMessage method.
+// It is continually sending any received messages to the fwd channel on the room type.
+func (c *client) read() {
+	for {
+		if _, msg, err := c.ReadMessage(); err == nil {
+			c.room.fwd <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+// write method continually accepts messages from the send channel writing everything
+// out of the websocket via the WriteMessage method.
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
 // room defines our chatroom.
 type room struct {
 	// fwd is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
 	fwd chan []byte
+	// join is a channel for clients in-transit to join this room.
+	join chan *client
+	// leave is a channel for joined clients wishing to leave the room.
+	leave chan *client
+	// clients holds all current clients in this room.
+	clients map[*client]bool
 }
